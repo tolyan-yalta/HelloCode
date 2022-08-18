6c9883f19043dd0cea5152404a527f283d74369e
anatoly <tolyan.yalta@yandex.ru>
2022-08-18 19:45:56 +0300

    Create file DogAndTwoFriends

---
+++ DogAndTwoFriends/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+double distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+double time = 0;
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++; 
+}
+Console.Write("Собака пробежит ");
+Console.WriteLine(count);
+Console.Write(" раз");
