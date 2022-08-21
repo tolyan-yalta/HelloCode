bbbc73c1d2152024dcc8a752ee7e75c769d2140d
anatoly <tolyan.yalta@yandex.ru>
2022-08-21 18:05:52 +0300

    Changed Example 008 IntroMethod

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -5,21 +5,21 @@
     if(arg3 > result) result = arg3;
     return result;
 }
-
-
 int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 23;
+int b2 = 231;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
 int c3 = 31;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
 
-int max = Max(max1, max2, max3);
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
 Console.WriteLine(max);
